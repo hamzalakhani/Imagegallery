238a5aa416928d2b2b5c8d62241407b0dc01982b
Hamza Lakhani <hamzalakhani10@gmail.com>
2016-11-14 14:52:07 -0500

    Initial Commit

---
+++ ImageGallery/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageGallery
+//
+//  Created by Hamza Lakhani on 2016-11-14.
+//  Copyright © 2016 Hamza Lakhani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageGallery/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImageGallery
+//
+//  Created by Hamza Lakhani on 2016-11-14.
+//  Copyright © 2016 Hamza Lakhani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
